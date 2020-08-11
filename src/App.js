601b1c3b6bd862fa3ad5f7bef7965a3ed2337e88
alvin lubetkin <alvin@terminal.co>
2020-08-11 02:34:11 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import TextField from "@material-ui/core/TextField";
-import MainContent from "./MainContent";
+
 import ExampleForm from "./ExampleForm";
 
 function App() {
-  //probably will end up being async
-
-  // const ponchNeedsToKnow = () => {
-  //   console.log("ponchis dumb");
-  //   // query backend here
-  // };
-
-  // const ponchTheTeacher = () => {
-  //   console.log(teachings);
-  //   const newTeaching = { item: "new_teaching", data: teachings };
-  //   //send new info to backend for storage
-  // };
-
-  // const onChange = () => {
-  //   o;
-  // };
-
+  console.log("running");
   return (
     <div className="App">
       <ExampleForm />
--- src/ExampleForm.js
+++ src/ExampleForm.js
@@ -3,26 +3,27 @@ import ReactQuill from "react-quill";
 import { Button, Input } from "@material-ui/core";
 import "react-quill/dist/quill.snow.css";
 import db from "./db";
-import data from "./roundsDataStructure";
+import { data } from "./roundsDataStructure";
 const ExampleForm = () => {
   //change/create state variables for each element in roundsDataStructure
-  const [conductivityP2, setValue] = useState("");
-  const [name, setName] = useState("");
-  const [conductivityP1, setConductivity] = useState("");
+  const [conductivityP2, setConductivityP2] = useState("");
+  const [conductivityP1, setConductivityP1] = useState("");
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
-    console.log("bitchass value: ", value);
+
     //upload to MongoDB
-    // let newData = dataHelper(condP1, condP2);
+    let newData = dataHelper();
+    console.log("new data: ", newData);
     // db.write("newDataName", newData);
   };
 
   const dataHelper = () => {
     let result;
     data[29].conductivity.p1 = conductivityP1;
+    data[29].conductivity.p2 = conductivityP2;
     //do this for the rest of elements in data structure
-    return result;
+    return data;
   };
 
   return (
@@ -31,22 +32,19 @@ const ExampleForm = () => {
       makes a label w input inside for each element in roundsDataStructure
       */}
       <label>
-        Machine Name:
+        Conductivity P1
         <input
           type="text"
-          value={conductivity}
-          onChange={(event) => setName(event.target.value)}
+          value={conductivityP1}
+          onChange={(event) => setConductivityP1(event.target.value)}
         />
       </label>
       <label>
-        Enter Instructions:
+        Conductivity P2{" "}
         <input
-          multiline={true}
-          rows={10}
-          rowsmax={50}
           type="text"
-          value={value}
-          onChange={(event) => setValue(event.target.value)}
+          value={conductivityP2}
+          onChange={(event) => setConductivityP2(event.target.value)}
         />
       </label>
       <input type="submit" value="Submit" />
